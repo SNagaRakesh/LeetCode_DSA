205822f39333102d54e9a717cb7949123e504dda
Rakesh Samudrala <nagarakeshsamudrala@gmail.com>
2026-08-22 08:22:27 +0530

    Time: 15 ms (58.96%), Space: 46.4 MB (39.16%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,34 +1,47 @@
 class Solution {
     public String longestPalindrome(String s) {
         int n = s.length();
-
-        int maxLength = 0;
         int start = -1;
         int end = -1;
 
-        for(int i = 0; i < n; i++) {
-            for(int j = i; j < n; j++) {
-                if(isPalindrome(s.substring(i, j+1))) {
-                    if(j - i + 1 > maxLength) {
-                        maxLength = Math.max(j-i+1, maxLength);
-                        start = i;
-                        end = j;
-                    }                    
-                }
-            }
-        }
-        return s.substring(start, end+1);
-    }
-    public static boolean isPalindrome(String s) {
-        int left = 0, right = s.length() - 1;
+        int maxLength = 0;
 
-        while(left <= right) {
-            if(s.charAt(left) != s.charAt(right)) {
-                return false;
+        for(int i = 0; i < n-1; i++) {
+            // odd palindrome
+            int left = i;
+            int right = i;
+            
+            int[] odd = isPalindrome(s, left, right);
+            
+            if (odd[1] - odd[0] + 1 > maxLength) {
+                maxLength = odd[1] - odd[0] + 1;
+                start = odd[0];
+                end = odd[1];
             }
-            left++;
-            right--;
+            
+            // even palindrome 
+            left = i;
+            right = i+1;
+
+            int[] even = isPalindrome(s, left, right);
+
+            if (even[1] - even[0] + 1 > maxLength) {
+                maxLength = even[1] - even[0] + 1;
+                start = even[0];
+                end = even[1];
+            }          
         }
-        return true;
+        return (start == -1 || end == -1) ? s.substring(0, 1) : s.substring(start, end + 1);
+    }
+    public static int[] isPalindrome(String s, int left, int right) {
+
+        while(left >= 0 && right < s.length()) {
+            if(s.charAt(left) != s.charAt(right)){
+                return new int[] {left + 1, right - 1};
+            }
+            left--;
+            right++;
+        }
+        return new int[] {left + 1, right - 1};
     }
 }
