99902e4df309e61cce2316705b243319651a2985
Rakesh Samudrala <nagarakeshsamudrala@gmail.com>
2026-09-06 19:59:00 +0530

    Time: 26 ms (99.64%), Space: 54.2 MB (99.48%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,24 +1,26 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> ans = new ArrayList<>();
-
         int n = nums.length;
+
+        List<List<Integer>> ans = new ArrayList<>();
 
         mergeSort(nums, 0, n-1);
 
         for(int i = 0; i < n; i++) {
-            if(i > 0 && nums[i] == nums[i-1]) continue;
-            int j = i+1, k = n-1;
+            if(i > 0 && nums[i-1] == nums[i]) continue;
+            int j = i + 1;
+            int k = n - 1;
 
             while(j < k) {
                 int sum = nums[i] + nums[j] + nums[k];
+
                 if(sum < 0) {
                     j++;
                 }
                 else if(sum > 0) {
                     k--;
                 }
-                else {
+                else{
                     List<Integer> temp = new ArrayList<>();
                     temp.add(nums[i]);
                     temp.add(nums[j]);
@@ -27,72 +29,45 @@ class Solution {
                     j++;
                     k--;
                     while(nums[j] == nums[j-1] && j < k) j++;
-                    while(nums[k] == nums[k+1] && j < k) k--;
+                    while(nums[k] == nums[k+1] && j < k) k--;                    
                 }
             }
         }
         return ans;
     }
-    public static void mergeSort(int[] arr, int left, int right) {
-        if (left < right) {
-            // Find the middle point safely to avoid overflow
-            int mid = left + (right - left) / 2;
-
-            // Sort first and second halves
-            mergeSort(arr, left, mid);
-            mergeSort(arr, mid + 1, right);
-
-            // Merge the sorted halves
-            merge(arr, left, mid, right);
+    public static void mergeSort(int[] nums, int low, int high) {
+        if(low < high) {
+            int mid = (low + (high - low) / 2);
+            mergeSort(nums, low, mid);
+            mergeSort(nums, mid + 1, high);
+            merge(nums, low, mid, high);
         }
     }
+    public static void merge(int[] nums, int low, int mid, int high) {
+        int n = nums.length;
 
-    // Conquer phase: Merges two sorted subarrays back into the original array
-    private static void merge(int[] arr, int left, int mid, int right) {
-        // Find sizes of two subarrays to be merged
-        int n1 = mid - left + 1;
-        int n2 = right - mid;
+        int[] temp = new int[high - low + 1];
+        int i = low;
+        int j = mid + 1;
+        int k = 0;
 
-        // Create temporary arrays
-        int[] leftArr = new int[n1];
-        int[] rightArr = new int[n2];
-
-        // Copy data to temporary arrays
-        for (int i = 0; i < n1; ++i) {
-            leftArr[i] = arr[left + i];
-        }
-        for (int j = 0; j < n2; ++j) {
-            rightArr[j] = arr[mid + 1 + j];
-        }
-
-        // Initial indexes of first and second subarrays
-        int i = 0, j = 0;
-
-        // Initial index of merged subarray
-        int k = left;
-        while (i < n1 && j < n2) {
-            if (leftArr[i] <= rightArr[j]) {
-                arr[k] = leftArr[i];
-                i++;
-            } else {
-                arr[k] = rightArr[j];
-                j++;
+        while(i <= mid && j <= high) {
+            if(nums[i] <= nums[j]) {
+                temp[k++] = nums[i++];
             }
-            k++;
+            else {
+                temp[k++] = nums[j++];
+            }
+        }
+        while(i <= mid) {
+            temp[k++] = nums[i++];
+        }
+        while(j <= high) {
+            temp[k++] = nums[j++];
         }
 
-        // Copy remaining elements of leftArr[] if any
-        while (i < n1) {
-            arr[k] = leftArr[i];
-            i++;
-            k++;
-        }
-
-        // Copy remaining elements of rightArr[] if any
-        while (j < n2) {
-            arr[k] = rightArr[j];
-            j++;
-            k++;
+        for(int a = 0, b = low; a < temp.length; a++, b++) {
+            nums[b] = temp[a];
         }
     }
 }
