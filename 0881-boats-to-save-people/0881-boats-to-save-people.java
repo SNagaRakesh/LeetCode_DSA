b51cda5ec0cc563b56f1c2705b548a46f6859ef9
Rakesh Samudrala <nagarakeshsamudrala@gmail.com>
2026-09-09 11:52:35 +0530

    Time: 19 ms (98.86%), Space: 56.4 MB (76.06%) - LeetHub

---
+++ 0881-boats-to-save-people/0881-boats-to-save-people.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int numRescueBoats(int[] people, int limit) {
+        int i = 0;
+        int j = people.length - 1;
+
+        int boats = 0;
+
+        Arrays.sort(people);
+
+        while(i <= j){
+            if(people[i] + people[j] <= limit) {
+                i++;
+            }
+            j--;
+            boats++;
+        }
+        return boats;
+    }
+}
